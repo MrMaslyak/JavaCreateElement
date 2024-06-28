99e0913428d2744f8d1ed43af8592901d376aa64
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-06-28 15:58:56 +0300

    done

--- src/Menu.java
+++ src/Menu.java
@@ -6,7 +6,7 @@ public class Menu extends JFrame {
 
     Menu() {
         setTitle("Menu");
-        setSize(500, 500);
+        setSize(315, 300);
         setLayout(null);
         ui();
         setVisible(true);
@@ -16,17 +16,33 @@ public class Menu extends JFrame {
     }
 
     public void ui() {
-        coordinat = new JLabel("0");
-        coordinat.setBounds(250, 270, 100, 30);
+        coordinat = new JLabel("0 %");
+        coordinat.setBounds(120, 150, 100, 30);
         coordinat.setFont(new Font("Arial", Font.ITALIC, 30));
         coordinat.setForeground(Color.red);
         add(coordinat);
 
-
-
-        ScrollBarElement scroll = new ScrollBarElement(100, 250, 125, 0, coordinat);
+        ScrollBarElement scroll = new ScrollBarElement(300, 150, 0, 0, coordinat);
         add(scroll);
 
+        JButton  buttonM = new JButton("<");
+        buttonM.setBounds(3, 150, 40, 35);
+        buttonM.setFont(new Font("Arial", Font.ITALIC, 10));
+        buttonM.setForeground(new Color(76, 7, 7));
+        buttonM.setBackground(new Color(234, 228, 228, 255));
+        buttonM.addActionListener(scroll);
+        add(buttonM);
+
+        JButton  buttonP = new JButton(">");
+        buttonP.setBounds(257, 150, 40, 35);
+        buttonP.setFont(new Font("Arial", Font.ITALIC, 10));
+        buttonP.setForeground(new Color(76, 7, 7));
+        buttonP.setBackground(new Color(234, 228, 228, 255));
+        buttonP.addActionListener(scroll);
+        add(buttonP);
+
+
     }
+
 
 }
--- src/ScrollBarElement.java
+++ src/ScrollBarElement.java
@@ -1,11 +1,14 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 
-public class ScrollBarElement extends JPanel implements MouseMotionListener {
+public class ScrollBarElement extends JPanel implements MouseMotionListener, ActionListener {
     private int heightLine = 10;
+    private int progress = 0;
     private int xLine = getWidth() / 2;
     private JLabel coordinat;
 
@@ -27,21 +30,24 @@ public class ScrollBarElement extends JPanel implements MouseMotionListener {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.green);
+        g.setColor(Color.blue);
         g.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2);
         g.drawLine(getWidth() / 5, getHeight() / 2 + heightLine, getWidth() / 5, getHeight() / 2 - heightLine);
         g.drawLine((getWidth() / 5) * 2, getHeight() / 2 + heightLine, (getWidth() / 5) * 2, getHeight() / 2 - heightLine);
         g.drawLine((getWidth() / 5) * 3, getHeight() / 2 + heightLine, (getWidth() / 5) * 3, getHeight() / 2 - heightLine);
         g.drawLine((getWidth() / 5) * 4, getHeight() / 2 + heightLine, (getWidth() / 5) * 4, getHeight() / 2 - heightLine);
+
         g.setColor(Color.red);
-        g.drawOval(xLine, getHeight() / 2 - 5, 10, 10);
+        g.drawOval(xLine-5, getHeight() / 2 - 5, 10, 10);
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
+        Graphics g = getGraphics();
         if (e.getX() > xLine && e.getX() < xLine + 10) {
             xLine = e.getX() - 5;
-            coordinat.setText(String.valueOf(xLine));
+            coordinat.setText(String.valueOf(xLine) + "%");
+            setProgress((int) ((double) xLine / (double) getWidth() * 100));
             repaint();
         }
     }
@@ -49,4 +55,34 @@ public class ScrollBarElement extends JPanel implements MouseMotionListener {
     @Override
     public void mouseMoved(MouseEvent e) {
     }
-}
+
+
+    public void setProgress(int progress) {
+        this.progress = Math.min(100, Math.max(0, progress));
+        coordinat.setText(this.progress + "%");
+        if ( progress < 0){
+            xLine = 0;
+            coordinat.setText("0%");
+        } else if (progress > 100) {
+            xLine = getWidth()-15;
+            coordinat.setText("100%");
+        }
+        repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        JButton button = (JButton) source;
+        if (button.getText().equals(">")) {
+            xLine += 15;
+            setProgress((int) ((double) xLine / (double) getWidth() * 100));
+            repaint();
+        }else if (button.getText().equals("<")) {
+            xLine -= 15;
+            setProgress((int) ((double) xLine / (double) getWidth() * 100));
+            repaint();
+        }
+    }
+    }
+
